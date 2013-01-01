e1897548d5b7e0a1af96655999fb2fe69ba39a91
GregKing_cp <gregking@users.noreply.github.com>
2013-01-01 01:49:23 +0000

    Updated some copyright lines.

--- CBM-Command/input.c
+++ CBM-Command/input.c
@@ -1,5 +1,5 @@
 /***************************************************************
-Copyright (c) 2011, Payton Byrd
+Copyright (c) 2012, Payton Byrd
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or
